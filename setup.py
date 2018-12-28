df47b9149c6c9ce934e8a84d3805fa6a39df9555
Aiken Pang <aikenpang123@hotmail.com>
2018-12-28 12:12:54 -0500

    Added setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import subprocess
+import sys
+import shutil
+import add_overlay
+
+setup(
+    name = "add_overlay",
+    version = add_overlay.__version__,
+    url = 'https://github.com/aikenpang/ultra96_overlayExample1',
+    license = 'All rights reserved.',
+    author = "Aiken Pang",
+    author_email = "aikenpang@gmail.com",
+    packages = ['add_overlay'],
+    package_data = {
+    '' : ['*.bit','*.tcl','*.py','*.so'],
+    },
+    description = "Example#1 Adder overlay for ULTRA96"
+)
